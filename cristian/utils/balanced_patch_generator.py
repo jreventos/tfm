ff0b6c2fd2ebff0b526c2d93a3c5003bfa82c776
cristianpachon <cc.pachon@gmail.com>
2020-11-06 16:24:57 +0000

    Reduce number of iterations in balanced_patch_generator

--- cristian/utils/balanced_patch_generator.py
+++ cristian/utils/balanced_patch_generator.py
@@ -6,7 +6,7 @@ import torch
 from torch.utils.data import Dataset
 
 class BalancedPatchGenerator(Dataset):
-    def __init__(self, images_path, masks_path, patch_shape, positive_prob = 0.7, shuffle_images = False):
+    def __init__(self, images_path, masks_path, patch_shape, positive_prob = 0.7, shuffle_images = False, is_test=False):
         self.images_path = images_path
         self.masks_path = masks_path
         self.patch_shape = patch_shape
@@ -14,6 +14,7 @@ class BalancedPatchGenerator(Dataset):
         self.shuffle_images = shuffle_images
         self.images_files = self.get_files(self.images_path)
         self.masks_files = self.get_files(self.masks_path)
+        self.is_test = is_test
         
         if self.shuffle_images:
             self.images_files = np.random.permutation(self.images_files)
@@ -26,10 +27,10 @@ class BalancedPatchGenerator(Dataset):
     def get_files(path):
         return [os.path.join(path, file) for file in os.listdir(path) if file.split('.')[-1] == 'vtk']
         
-    @staticmethod
-    def get_shape(image_location):
-        image = VtkReader(image_location)
-        return image.shape
+#    @staticmethod
+#    def get_shape(image_location):
+#        image = VtkReader(image_location)
+#        return image.shape
     
     def get_patient_identifier(self, idx):
         image_path = self.images_files[idx]
@@ -62,7 +63,7 @@ class BalancedPatchGenerator(Dataset):
         return tuple(idxs_list)
 
     @staticmethod
-    def get_indexes(self, tensor, value, patch_size, num_attempts=40):
+    def get_indexes(self, tensor, value, patch_size, num_attempts=3):
         i = 0
         best_patch_coverage = 0
         best_idxs = None
@@ -76,29 +77,36 @@ class BalancedPatchGenerator(Dataset):
             i+=1
         return best_idxs
         
+#    def __len__(self):
+#        total_patients = self.num_patients
+#        percentage_10 = int(np.minimum(np.maximum(np.ceil(0.1*total_patients), 2), total_patients))  
+#        first_10_images = np.random.permutation(self.images_files)[:percentage_10]
+#        images_shape = np.array([np.array(self.get_shape(image)) for image in first_10_images])
+#        mean_shape = np.mean(images_shape, axis=0).astype(int)
+#        total_dimensions = len(mean_shape)
+#        total_patches = 1
+#        for i in range(total_dimensions):
+#            total_patches *= (mean_shape[i] - self.patch_shape[i] + 1)
+#        return total_patches * self.num_patients
     def __len__(self):
-        total_patients = self.num_patients
-        percentage_10 = int(np.minimum(np.maximum(np.ceil(0.1*total_patients), 2), total_patients))  
-        first_10_images = np.random.permutation(self.images_files)[:percentage_10]
-        images_shape = np.array([np.array(self.get_shape(image)) for image in first_10_images])
-        mean_shape = np.mean(images_shape, axis=0).astype(int)
-        total_dimensions = len(mean_shape)
-        total_patches = 1
-        for i in range(total_dimensions):
-            total_patches *= (mean_shape[i] - self.patch_shape[i] + 1)
-        return total_patches * self.num_patients
+        return self.num_patients
     
     def __getitem__(self, idx):
-        image_idx = idx % self.num_patients
+        #image_idx = idx % self.num_patients
+        image_idx = idx
         patient_id = self.get_patient_identifier(image_idx)
         image_path, mask_path = self.get_image_mask_path(patient_id)
         image = VtkReader(image_path)
         mask = VtkReader(mask_path)
         value = 1 if np.random.rand(1)[0] <= self.positive_prob else 0
-        print(value)
         idxs = self.get_indexes(self, mask, value, self.patch_shape)
         im_patch = image[idxs]
         im_patch = np.expand_dims(im_patch, axis=0)
+        im_patch = torch.from_numpy(im_patch.astype(np.float32))
         mask_patch = mask[idxs]
         mask_patch = np.expand_dims(mask_patch, axis=0)
-        return im_patch, mask_patch 
+        mask_patch = torch.from_numpy(mask_patch.astype(np.float32))
+        if self.is_test:
+            return value, im_patch, mask_patch 
+        else:
+            return im_patch, mask_patch 
