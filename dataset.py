65f08429788fbfb399c7a12fbd51c31a7bff8d33
jreventos <janareventos13@gmail.com>
2020-11-08 11:06:16 +0100

    balanced dataset

--- dataset.py
+++ dataset.py
@@ -210,9 +210,9 @@ class PatchesDataset_2(Dataset):
             #mask_patch = self.transform(mask_patch.astype(np.float32))
 
         # If validation mode no patch is extracted
-        if self.mode == 'val':
-            vol_patch = volume_array
-            mask_patch = mask_array
+        # if self.mode == 'val':
+        #     vol_patch = volume_array
+        #     mask_patch = mask_array
 
         # Add channel dimension in volumes and masks
         vol_patch = vol_patch[None, :, :, :]
@@ -348,50 +348,153 @@ class LeftAtriumSegmentationDataset(Dataset):
         return image_tensor, mask_tensor
 
 
+class BalancedPatchGenerator(Dataset):
+    def __init__(self, path, patch_shape, positive_prob=0.7, shuffle_images=False, mode=None, is_test=False,
+                 transform=None):
+
+        self.path = path
+        self.patch_shape = patch_shape
+        self.positive_prob = positive_prob
+        self.shuffle_images = shuffle_images
+        self.mode = mode
+        self.is_test = is_test
+        assert self.mode in ['train', 'val']
+        self.images_files = self.get_files(self.path, self.mode, 'MRI_volumes')
+        self.masks_files = self.get_files(self.path, self.mode, 'masks')
+        self.transform = transform
+
+        if self.shuffle_images:
+            self.images_files = np.random.permutation(self.images_files)
+
+        assert len(self.images_files) == len(self.masks_files), 'Num.images is different from num. masks'
+
+        self.num_patients = len(self.images_files)
+
+    @staticmethod
+    def get_files(path, mode, type_files):
+        return [os.path.join(os.path.join(os.path.join(path, type_files), mode), file) for file in
+                os.listdir(os.path.join(os.path.join(path, type_files), mode)) if file.split('.')[-1] == 'vtk']
+
+    #    @staticmethod
+    #    def get_shape(image_location):
+    #        image = VtkReader(image_location)
+    #        return image.shape
+
+    def get_patient_identifier(self, idx):
+        image_path = self.images_files[idx]
+        path, filename = os.path.split(image_path)
+        name, extension = filename.split('.')
+        identifier = int(name.split('_')[1])
+        return identifier
+
+    def get_image_mask_path(self, patient_id):
+        img_pattern = re.compile('MRI_{}'.format(patient_id))
+        image_path = list(filter(img_pattern.search, self.images_files))[0]
+        mask_pattern = re.compile('mask_{}'.format(patient_id))
+        mask_path = list(filter(mask_pattern.search, self.masks_files))[0]
+        return image_path, mask_path
+
+    @staticmethod
+    def get_candidate_indexes(tensor, value, patch_size):
+        idxs_list = []
+        idxs_condition = np.where(tensor == value)
+        position = np.random.randint(len(idxs_condition[0]))
+        suggested_initial_points = [idxs_condition[i][position] for i in range(len(tensor.shape))]
+        for i in range(len(suggested_initial_points)):
+            ini = suggested_initial_points[i]
+            total_pixels = tensor.shape[i]
+            patch_pixels = patch_size[i]
+            if ini + patch_pixels >= total_pixels:
+                ini = np.maximum(total_pixels - patch_pixels - 1, 0)
+            end = ini + patch_pixels
+            idxs_list.append(slice(ini, end, 1))
+        return tuple(idxs_list)
+
+    @staticmethod
+    def get_indexes(self, tensor, value, patch_size, num_attempts=3):
+        i = 0
+        best_patch_coverage = 0
+        best_idxs = None
+        while i < num_attempts:
+            idxs = self.get_candidate_indexes(tensor, value, patch_size)
+            patch_candidate = tensor[idxs]
+            patch_coverage = np.sum(patch_candidate == value) / np.prod(patch_candidate.shape)
+            if patch_coverage > best_patch_coverage:
+                best_patch_coverage = patch_coverage
+                best_idxs = idxs
+            i += 1
+        return best_idxs
+
+    def __len__(self):
+        return self.num_patients
+
+    def __getitem__(self, idx):
+
+        # get patient identifier:
+        image_idx = idx
+        patient_id = self.get_patient_identifier(image_idx)
+        image_path, mask_path = self.get_image_mask_path(patient_id)
+
+        # read arrays
+        image = VtkReader(image_path)[103:223, 110:230, 10:59]
+        mask = VtkReader(mask_path)[103:223, 110:230, 10:59]
+
+        # compute a value between 0 and 1 to
+        value = 1 if np.random.rand(1)[0] <= self.positive_prob else 0
+        idxs = self.get_indexes(self, mask, value, self.patch_shape)
+
+        # get patches
+        im_patch = image[idxs]
+        mask_patch = mask[idxs]
+
+        # in case of transform (just for the train mode)
+        if self.transform is not None:
+            im_patch = self.transform(im_patch.astype(np.float32))
+
+        # expand dimensions
+        im_patch = np.expand_dims(im_patch, axis=0)
+        mask_patch = np.expand_dims(mask_patch, axis=0)
+
+        # tensor
+        im_patch = torch.from_numpy(im_patch.astype(np.float32))
+        mask_patch = torch.from_numpy(mask_patch.astype(np.float32))
+
+        if self.is_test:
+            return value, im_patch, mask_patch
+        else:
+            return im_patch, mask_patch
 
 
 
-# images_path = "/Users/jreventos/Desktop/TFM/tfm/patients_data/MRI_volumes/val"
-# masks_path = "/Users/jreventos/Desktop/TFM/tfm/patients_data/masks/val"
-#
-# ddd = GeneratePatches(images_path,
-#                       masks_path,
-#                       (16, 16, 6),
-#                       shuffle_images=True,
-#                       allow_overlap=True)
-#
-# for p in ddd.patients_information:
-#     print(p)
-#
-# loader = DataLoader(ddd, batch_size=1, shuffle=False)
-# print('Length DataLoader:', len(loader))
-# for i, data in enumerate(loader):
-#     x, y_true = data
-#     print('Volume shape', i, ':', x.shape)
-#     print('Mask shape', i, ':', y_true.shape)
+
 
 
 if __name__ == "__main__":
 
 
-    root_dir = "/Users/jreventos/Desktop/TFM/tfm/patients_data2"
-    val_dataset = PatchesDataset_2(root_dir,
-                                 transform=None,
-                                 patch_dim=[60, 60, 36],
-                                 num_patches=1,
-                                 mode='val',
-                                 random_sampling=False)
-
-    # val_dataset = PatchesDataset(root_dir,
+    path = "/Users/jreventos/Desktop/TFM/tfm/patients_data2"
+    # val_dataset = PatchesDataset_2(path,
     #                              transform=None,
-    #                              patch_dim= [36, 36, 36],
-    #                              patch_stepsize= 64,
-    #                              patch_function=volume_patches,
+    #                              patch_dim=[60, 60, 36],
+    #                              num_patches=1,
     #                              mode='val',
     #                              random_sampling=False)
+    #
+    #
+    # loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
 
-    loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+    ddd = BalancedPatchGenerator(path,
+                                 (32, 32, 32),
+                                 positive_prob=0.7,
+                                 shuffle_images=True,
+                                 mode='val',
+                                 is_test=False,
+                                 transform=None)
+
+    loader = DataLoader(ddd, batch_size=1, shuffle=False)
+
+
 
 
     for i, data in enumerate(loader):
